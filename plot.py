7dbcc72c1ffaaa384f241bd6529d9b6229ce00fb
MFukuoka <pochi.masahiro@gmail.com>
2025-10-20 02:47:21 +0900

    plot

--- plot.py
+++ plot.py
@@ -7,12 +7,21 @@ raw = []
 with open('./corr.dat', 'r') as f:
 	for n in f:
 		newline = n.rstrip('\n').split(',')
-		print(newline)
-		sat = newline[0].rstrip(' ,').strip(' ')
-		phase = newline[1].rstrip(' ,').strip(' ')
-		corr = newline[2].strip(' ').rstrip(' \n')
+		sat = int(newline[0].rstrip('  ,').strip('  '))
+		phase = int(newline[1].rstrip('  ,').strip('  '))
+		corr = int(newline[2].strip('  ').rstrip('  \n'))
 		raw.append([sat, phase, corr])
 
+corr_data = np.array(raw)
 
-print(np.array(raw))
+print(corr_data)
 
+for n in range(1,33):
+	sat1 = np.where(corr_data[:,0] == n)[0]
+	print(n)
+	print(sat1)
+	print(corr_data[sat1, 1])
+	print(corr_data[sat1, 2])
+
+	plt.plot(corr_data[sat1, 1], corr_data[sat1, 2])
+	plt.show()
