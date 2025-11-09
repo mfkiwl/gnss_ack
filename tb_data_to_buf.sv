ece995ca4d796756b34184072f49561be028d894
MFukuoka <pochi.masahiro@gmail.com>
2025-11-10 02:20:34 +0900

    ram

--- tb_data_to_buf.sv
+++ tb_data_to_buf.sv
@@ -6,13 +6,8 @@ logic rst;
 always #10 clk = ~clk;
 always #100 adc_clk = ~adc_clk;
 
-logic [9:0] i;
-logic [9:0] q;
-
-logic [35:0] dataout_i;
-logic [35:0] datain_i;
-logic [13:0] address;
-logic write_enable;
+logic i;
+logic q;
 
 bsram_18k_36 bbi
 (
@@ -24,6 +19,58 @@ bsram_18k_36 bbi
     .CLK(1'd0),
     .RESET(1'd0)
 );
+
+
+logic [1:0] adc_clk_reg;
+logic [1:0] i_sreg;
+logic [1:0] q_sreg;
+always_ff @(posedge clk)
+begin
+    adc_clk_reg <= {adc_clk_reg[0], adc_clk};
+    i_sreg <= {i_sreg[0], i};
+    q_sreg <= {q_sreg[0], q};
+end
+
+logic adc_clk_flag;
+assign adc_clk_flag = ~adc_clk_reg[1] & adc_clk_reg[0];
+
+logic [35:0] dataout_i;
+logic [35:0] datain_i;
+logic [13:0] address;
+logic write_enable;
+
+logic [7:0] buf_count;
+always_ff @(posedge clk or negedge rst)
+begin
+    if (!rst)
+    begin
+        datain_i <= 36'd0;
+        address <= 14'd0;
+        buf_count <= 8'd0;
+        write_enable <= 1'b0;
+    end
+    else
+    begin
+        if (adc_clk_flag)
+        begin
+            datain_i <= {i, datain_i[35:1]};
+
+            if (buf_count < 8'd35)
+            begin
+                buf_count <= buf_count + 8'd1;
+                write_enable <= 1'b0;
+            end
+            else
+            begin
+                buf_count <= 8'd0;
+                write_enable <= 1'b1;
+            end
+        end
+    end
+end
+
+
+
 
 integer rnum;
 integer fd;
@@ -39,19 +86,23 @@ $dumpfile("test.vcd");
 $dumpvars(2, tb);
 $dumpall;
 $dumpon;
+rst = 1'b1;
 num = 0;
 clk = 1'b0;
 clk = 1'b0;
 adc_clk = 1'b0;
+#13;
+rst = 1'b0;
+#13;
+rst = 1'b1;
 fd = $fopen("./L1_20211202_084700_4MHz_IQ.bin", "rb");
-repeat (20) @(negedge adc_clk)
+repeat (1027) @(negedge adc_clk)
 begin
-    @(posedge clk);
     rnum = $fread(tmp1, fd);
     rnum = $fread(tmp2, fd);
 
-    i[num] = tmp1[2];
-    q[num] = tmp2[2];
+    i = tmp1[2];
+    q = tmp2[2];
     num = num + 1;
 end
 $finish;
