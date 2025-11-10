2e31e7cb2939d8cb8d6dac26216bfca0ccc927f0
M. Fukuoka <fukuoka-masahiro@ed.tmu.ac.jp>
2025-11-10 22:41:33 +0900

    hoge

--- tb_data_to_buf.sv
+++ tb_data_to_buf.sv
@@ -12,7 +12,7 @@ logic q;
 bsram_18k_36 bbi
 (
     .DO(dataout_i),
-    .DI(datain_i),
+    .DI({gomi, datain_i}),
     .AD(address),
     .WRE(write_enable),
     .CE(1'd0),
@@ -34,37 +34,38 @@ end
 logic adc_clk_flag;
 assign adc_clk_flag = ~adc_clk_reg[1] & adc_clk_reg[0];
 
+logic [27:0] gomi;
+
 logic [35:0] dataout_i;
-logic [35:0] datain_i;
+logic [7:0] datain_i;
 logic [13:0] address;
 logic write_enable;
 
-logic [7:0] buf_count;
+logic capture;
+
+logic [2:0] buf_count;
 always_ff @(posedge clk or negedge rst)
 begin
     if (!rst)
     begin
-        datain_i <= 36'd0;
+        datain_i <= 8'd0;
         address <= 14'd0;
-        buf_count <= 8'd0;
+        buf_count <= 3'd7;
         write_enable <= 1'b0;
     end
     else
     begin
-        if (adc_clk_flag)
+        if (capture)
         begin
-            datain_i <= {i, datain_i[35:1]};
+            if (adc_clk_flag)
+            begin
+                datain_i <= {i, datain_i[7:1]};
 
-            if (buf_count < 8'd35)
-            begin
-                buf_count <= buf_count + 8'd1;
-                write_enable <= 1'b0;
+                if (buf_count < 3'd7) buf_count <= buf_count + 3'd1;
+                else buf_count <= 3'd0;
             end
-            else
-            begin
-                buf_count <= 8'd0;
-                write_enable <= 1'b1;
-            end
+            if (buf_count == 3'd7) write_enable <= 1'b0;
+            else write_enable <= 1'b1;
         end
     end
 end
@@ -90,23 +91,33 @@ rst = 1'b1;
 num = 0;
 clk = 1'b0;
 clk = 1'b0;
-adc_clk = 1'b0;
+adc_clk = 1'b1;
 #13;
 rst = 1'b0;
 #13;
 rst = 1'b1;
-fd = $fopen("./L1_20211202_084700_4MHz_IQ.bin", "rb");
-repeat (1027) @(negedge adc_clk)
-begin
-    rnum = $fread(tmp1, fd);
-    rnum = $fread(tmp2, fd);
 
-    i = tmp1[2];
-    q = tmp2[2];
-    num = num + 1;
-end
+@(posedge adc_clk);
+@(negedge adc_clk);
+capture = 1'd1;
+repeat (500) @(posedge adc_clk);
 $finish;
 end
+
+initial
+begin
+    fd = $fopen("./L1_20211202_084700_4MHz_IQ.bin", "rb");
+    repeat (1027) @(negedge adc_clk)
+    begin
+        rnum = $fread(tmp1, fd);
+        rnum = $fread(tmp2, fd);
+
+        i = tmp1[2];
+        q = tmp2[2];
+        num = num + 1;
+    end
+end
+
 
 endmodule
 
