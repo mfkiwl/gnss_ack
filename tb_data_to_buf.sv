6efb37c70dcfa66792085ef627caaa276e19dd3f
MFukuoka <pochi.masahiro@gmail.com>
2025-11-14 02:40:12 +0900

    buf

--- tb_data_to_buf.sv
+++ tb_data_to_buf.sv
@@ -12,9 +12,20 @@ logic q;
 bsram_18k_36 bbi
 (
     .DO(dataout_i),
-    .DI({gomi, datain_i}),
-    .AD(address),
-    .WRE(write_enable),
+    .DI(datain_i),
+    .AD({address, 5'd0}),
+    .WRE(write_enable_0),
+    .CE(1'd0),
+    .CLK(clk),
+    .RESET(1'd0)
+);
+
+bsram_18k_36 bbq
+(
+    .DO(dataout_q),
+    .DI(datain_q),
+    .AD({address, 5'd0}),
+    .WRE(write_enable_0),
     .CE(1'd0),
     .CLK(clk),
     .RESET(1'd0)
@@ -37,22 +48,24 @@ assign adc_clk_flag = ~adc_clk_reg[1] & adc_clk_reg[0];
 logic [27:0] gomi;
 
 logic [35:0] dataout_i;
-logic [7:0] datain_i;
-logic [13:0] address;
-logic write_enable;
+logic [35:0] datain_i;
+logic [35:0] dataout_q;
+logic [35:0] datain_q;
+logic [8:0] address;
+logic write_enable_0;
 
 logic capture;
-logic [2:0] buf_count;
+logic [5:0] buf_count;
 logic flag_buf_count;
 
 always_ff @(posedge clk or negedge rst)
 begin
     if (!rst)
     begin
-        datain_i <= 8'd0;
-        address <= 14'd0;
-        buf_count <= 3'd0;
-        write_enable <= 1'b0;
+        datain_i <= 36'd0;
+        datain_q <= 36'd0;
+        address <= 9'd0;
+        buf_count <= 6'd0;
     end
     else
     begin
@@ -61,8 +74,18 @@ begin
             if (adc_clk_flag)
             begin
                 datain_i[buf_count] <= i;
+                datain_q[buf_count] <= q;
 
-                {flag_buf_count, buf_count} <= buf_count + 4'd1;
+                if (buf_count < 6'd35)
+                begin
+                    buf_count <= buf_count + 6'd1;
+                    flag_buf_count <= 1'b0;
+                end
+                else
+                begin
+                    buf_count <= 6'd0;
+                    flag_buf_count <= 1'b1;
+                end
             end
         end
     end
@@ -71,17 +94,18 @@ end
 logic [1:0] flag_reg;
 always_ff @(posedge clk or negedge rst)
 begin
-    if (!rst) write_enable <= 1'd0;
+    if (!rst)
+    begin
+        write_enable_0 <= 1'd0;
+    end
     else
     begin
         flag_reg <= {flag_reg[0], flag_buf_count};
-        if (flag_reg[1] == 1'd0 && flag_reg[0] == 1'd1) write_enable <= 1'd1;
-        else write_enable <= 1'd0;
-        if (flag_reg[1] == 1'd1 && flag_reg[0] == 1'd0) address <= address + 13'd1;
+        if (flag_reg[1] == 1'd0 && flag_reg[0] == 1'd1) write_enable_0 <= 1'd1;
+        else write_enable_0 <= 1'd0;
+        if (flag_reg[1] == 1'd1 && flag_reg[0] == 1'd0) address <= address + 9'd1;
     end
 end
-
-
 
 integer rnum;
 integer fd;
@@ -90,6 +114,8 @@ logic [7:0] tmp1;
 logic [7:0] tmp2;
 
 logic adc_clk;
+
+integer ii;
 
 initial
 begin
@@ -110,14 +136,28 @@ rst = 1'b1;
 @(posedge adc_clk);
 @(negedge adc_clk);
 capture = 1'd1;
-repeat (500) @(posedge adc_clk);
+
+@(posedge (address == 9'd112));
+
+capture = 1'd0;
+
+for (ii = 0; ii < 112; ii++)
+begin
+	address = ii[8:0];
+	@(posedge clk);
+	@(posedge clk);
+	$display("AD = %d, i = %h", address, dataout_i);
+	$display("AD = %d, q = %h", address, dataout_q);
+end
+
+
 $finish;
 end
 
 initial
 begin
     fd = $fopen("./L1_20211202_084700_4MHz_IQ.bin", "rb");
-    repeat (1027) @(negedge adc_clk)
+    repeat (200000) @(negedge adc_clk)
     begin
         rnum = $fread(tmp1, fd);
         rnum = $fread(tmp2, fd);
